ff03f52490cc55955ed6b4bb73a3916ee0a1ae83
ashurohi <46756327+ashurohi@users.noreply.github.com>
2019-01-16 11:48:45 -0500

    Add files via upload

---
+++ Hello1.java
@@ -0,0 +1,25 @@
+/***************************************************************************************************************************
+* Java Program Name
+***************************************************************************************************************************
+* Project/Program description
+*__________________________________________________________________________________________________________________________
+* Student First Name Last Name
+* Date
+* Course Number and Section
+***************************************************************************************************************************/
+
+public class Hello1 {
+    public static void printheading() {
+        System.out.println("Your Name");
+        System.out.println("Java Project Name");
+        System.out.println("Project Description");
+        System.out.println("Version Date");
+        System.out.println("Course Number and Section");
+        System.out.println();
+    }
+    
+    public static void main(String [] args) {
+        printheading();
+        System.out.println("Hello");
+    }
+}
